4c4ca5445420d8510275e3c7a1c627d3a1b2b15f
Kanon123 <baldeonbrian@gmail.com>
2023-10-03 23:50:25 -0300

    ultimas modificaciones

--- src/baseDatos/estudianteBD.js
+++ src/baseDatos/estudianteBD.js
@@ -4,12 +4,12 @@ const buscarPorId = async (idEstudiante) => {
 
     const consulta = `SELECT  dni, nombre, apellido,
     (CASE
-        WHEN nacionalidad = 0 THEN 'arg'
-        WHEN nacionalidad = 1 THEN 'uru'
-        WHEN nacionalidad = 2 THEN 'chi'
-        WHEN nacionalidad = 3 THEN 'par'
-        WHEN nacionalidad = 4 THEN 'bra'
-        WHEN nacionalidad = 5 THEN 'bol'
+        WHEN nacionalidad = 0 THEN 'argentino'
+        WHEN nacionalidad = 1 THEN 'uruguayo'
+        WHEN nacionalidad = 2 THEN 'chileno'
+        WHEN nacionalidad = 3 THEN 'paraguayo'
+        WHEN nacionalidad = 4 THEN 'brasilero'
+        WHEN nacionalidad = 5 THEN 'boliviano'
         ELSE ''
     END) AS nacionalidad 
     FROM estudiante 
@@ -55,13 +55,14 @@ const nuevo = async (estudiante) => {
     return buscarPorId(estudianteNuevo.insertId);
 }
 
-const update = async(idEstudiante ,{dni,nombre,apellido})=>{
-    const consulta = `UPDATE estudiante SET dni = ?, nombre = ?, apellido =? WHERE idEstudiante = ?`;
-    await conexion.query(consulta, [dni,nombre,apellido, idEstudiante])
+const update = async(idEstudiante ,{dni,nombre,apellido,fechaNacimiento,nacionalidad,correoElectronico,celular})=>{
+    const consulta = `UPDATE estudiante SET dni = ?, nombre = ?, apellido =?,fechaNacimiento=?, nacionalidad=?, correoElectronico=?,celular=? WHERE idEstudiante = ?`;
+    await conexion.query(consulta, [dni,nombre,apellido,fechaNacimiento,nacionalidad, correoElectronico,celular,idEstudiante])
 
     return buscarPorId(idEstudiante)
 
 }
+
 
 
 module.exports = {
