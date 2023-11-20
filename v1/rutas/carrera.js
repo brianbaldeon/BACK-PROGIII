ca382b9165e7ffb9b6d406cf5cef41a1e83659e6
cristian9821 <cristianpereyra9821@gmail.com>
2023-11-19 21:07:27 -0300

    checks id carrera

--- v1/rutas/carrera.js
+++ v1/rutas/carrera.js
@@ -13,10 +13,14 @@ router.post('/carreras', [
     ], crear);
 
 //eliminar
-router.delete('/carreras/:idCarrera', eliminar);
+router.delete('/carreras/:idCarrera', [
+    check('idCarrera', 'El id de carrera es requerido').not().isEmpty(),
+        validarCampos
+], eliminar);
 
 //actualizar
 router.put('/carreras/:idCarrera', [
+    check('idCarrera', 'El id de carrera es requerido').not().isEmpty(),
     check('nombre','El nombre es requerido').not().isEmpty(),
     check('modalidad', 'La modalidad es requerida').not().isEmpty(),
         validarCampos   
@@ -26,7 +30,10 @@ router.put('/carreras/:idCarrera', [
 router.get('/carreras', buscarTodos);
 
 //buscar Por ID
-router.get('/carreras/:idCarrera', buscarPorId);
+router.get('/carreras/:idCarrera', [
+    check('idCarrera', 'El id de carrera es requerido').not().isEmpty(),
+        validarCampos
+], buscarPorId);
 
 
 
